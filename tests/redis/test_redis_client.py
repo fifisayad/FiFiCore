cbb83bb5413fa48c7b0c1ce0a527e7ce6590e9a1
fifisayad <sayad.mehrdad@gmail.com>
2025-06-04 19:23:25 +0330

    feat: add redis pub-sub test

--- tests/redis/test_redis_client.py
+++ tests/redis/test_redis_client.py
@@ -1,6 +1,22 @@
+import asyncio
 import pytest
+
+from src.fifi import RedisSubscriber, RedisPublisher
+
+
+CHANNEL: str = "test_channel"
 
 
 @pytest.mark.redis
-def test_create_redis_client():
-    assert 1 == 2
+@pytest.mark.asyncio
+async def test_create_redis_client():
+    publisher = await RedisPublisher.create(CHANNEL)
+    subscriber = await RedisSubscriber.create(CHANNEL)
+
+    test = {"data": "test"}
+    await publisher.publish(test)
+
+    await asyncio.sleep(1)
+    recieved_test = await subscriber.get_last_message()
+
+    assert test == recieved_test
