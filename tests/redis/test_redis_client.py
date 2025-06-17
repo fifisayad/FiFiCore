6f916c2b00f83c6c8975a5a9bd36e3a2499d85ea
fifisayad <sayad.mehrdad@gmail.com>
2025-06-18 02:38:34 +0330

    fix: implement setUp, tearDown for test

--- tests/redis/test_redis_client.py
+++ tests/redis/test_redis_client.py
@@ -1,17 +1,29 @@
 import asyncio
 import pytest
+import pytest_asyncio
 
 from src.fifi import RedisSubscriber, RedisPublisher
+from src.fifi import GetLogger
 
 
 CHANNEL: str = "test_channel"
+LOGGER = GetLogger().get()
+
+@pytest_asyncio.fixture
+async def setup_redis_subscriber():
+    LOGGER.info("Creating Redis Subscriber ....")
+    subscriber = await RedisSubscriber.create(CHANNEL)
+    yield subscriber
+    LOGGER.info("Cleaning Redis Subscriber Task ...")
+    subscriber.close()
 
 
 @pytest.mark.redis
 @pytest.mark.asyncio
-async def test_create_redis_client():
+async def test_create_redis_client(setup_redis_subscriber):
     publisher = await RedisPublisher.create(CHANNEL)
-    subscriber = await RedisSubscriber.create(CHANNEL)
+    subscriber = setup_redis_subscriber
+    await asyncio.sleep(1)
 
     test = {"data": "test"}
     await publisher.publish(test)
@@ -19,4 +31,5 @@ async def test_create_redis_client():
     await asyncio.sleep(1)
     recieved_test = await subscriber.get_last_message()
 
+    LOGGER.info(f"Recieved Message: {recieved_test}")
     assert test == recieved_test
