cc01eaac3c4096591c65079c66b2b322a027a121
fifisayad <sayad.mehrdad@gmail.com>
2025-07-02 20:20:13 +0330

    feat: add new exceptions for repository

---
+++ src/fifi/exceptions/__init__.py
@@ -0,0 +1,8 @@
+__all__ = [
+    "RedisChannelSubException",
+    "FiFiException",
+    "IntegrityConflictException",
+    "NotFoundException",
+]
+
+from .exceptions import *
--- src/fifi/exceptions/exceptions.py
+++ src/fifi/exceptions/exceptions.py
@@ -1,2 +1,14 @@
 class RedisChannelSubException(Exception):
     pass
+
+
+class FiFiException(Exception):
+    pass
+
+
+class IntegrityConflictException(Exception):
+    pass
+
+
+class NotFoundException(Exception):
+    pass
