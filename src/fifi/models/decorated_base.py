b094726775f2937c61a614cfdedd24ca07c57525
fifisayad <sayad.mehrdad@gmail.com>
2025-07-02 22:04:18 +0330

    feat: add id column with uuid for the base class

--- src/fifi/models/decorated_base.py
+++ src/fifi/models/decorated_base.py
@@ -1,9 +1,13 @@
 from typing import Set
+import uuid
+from sqlalchemy import Column, String
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 
 
 class DecoratedBase(AsyncAttrs, DeclarativeBase):
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+
     def to_dict(self, exclude: Set = set()):
         return {
             column.name: getattr(self, column.name)
