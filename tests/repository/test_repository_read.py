58935efc3c626199c4dc19283acf2d3939df41f9
fifisayad <sayad.mehrdad@gmail.com>
2025-07-06 03:29:54 +0330

    feat: add get by id exception on column test

--- tests/repository/test_repository_read.py
+++ tests/repository/test_repository_read.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.fifi.exceptions import FiFiException
 from src.fifi import Repository
 from tests.repository.materials import *
 
@@ -8,7 +9,7 @@ from tests.repository.materials import *
 class TestRepositoryRead:
     user_repo = Repository(UserModel)
 
-    async def test_read_by_id_repository(self, database_provider_test, user_factory):
+    async def test_read_by_id(self, database_provider_test, user_factory):
         new_user_schema = user_factory()
         new_user = await self.user_repo.create(data=new_user_schema)
 
@@ -17,3 +18,10 @@ class TestRepositoryRead:
         LOGGER.info(f"got user model is: {got_user.to_dict()}")
 
         assert got_user.to_dict() == new_user.to_dict()
+
+    async def test_read_by_id_column_exception(
+        self, database_provider_test, user_factory
+    ):
+        id_ = "example"
+        with pytest.raises(FiFiException):
+            await self.user_repo.get_one_by_id(id_=id_, column="uuid")
