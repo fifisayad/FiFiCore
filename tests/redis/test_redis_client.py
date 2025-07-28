1a04a7e40d16a4df4c9512b7361b6e8a6fb88826
fifisayad <sayad.mehrdad@gmail.com>
2025-07-28 21:25:38 +0330

    feat: add test for redis performance

--- tests/redis/test_redis_client.py
+++ tests/redis/test_redis_client.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 import pytest
 import pytest_asyncio
 
@@ -37,3 +38,37 @@ async def test_create_redis_client(setup_redis_subscriber):
 
     LOGGER.info(f"Recieved Message: {recieved_test}")
     assert test == recieved_test
+
+
+@pytest.mark.redis
+@pytest.mark.asyncio
+async def test_performance(setup_redis_subscriber):
+    publisher = await RedisPublisher.create(CHANNEL)
+    subscriber = setup_redis_subscriber
+    await asyncio.sleep(1)
+
+    test = {
+        "data": {
+            "T": 1681924499999,
+            "c": "29258.0",
+            "h": "29309.0",
+            "i": "15m",
+            "l": "29250.0",
+            "n": 189,
+            "o": "29295.0",
+            "s": "BTC",
+            "t": 1681923600000,
+            "v": "0.98639",
+        },
+        "time": time.time(),
+    }
+    await publisher.publish(test)
+
+    await asyncio.sleep(1)
+    recieved_test = await subscriber.get_last_message()
+
+    assert test == recieved_test
+    circulation_time = time.time() - recieved_test["time"] - 1
+    LOGGER.info(f"{circulation_time=}")
+
+    assert circulation_time < 0.1
