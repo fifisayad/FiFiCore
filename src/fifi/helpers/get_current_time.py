f67c0994759575b3bd53edd6ad27ffabb7664f75
fifisayad <sayad.mehrdad@gmail.com>
2025-08-06 12:37:04 +0330

    fix: remove time zone from get datetime

--- src/fifi/helpers/get_current_time.py
+++ src/fifi/helpers/get_current_time.py
@@ -4,4 +4,4 @@ from datetime import datetime, UTC
 class GetCurrentTime:
     @staticmethod
     def get() -> datetime:
-        return datetime.now(UTC)
+        return datetime.now(UTC).replace(tzinfo=None)
