473569cd5e06cb2139e22b460e15081575cd70e8
fifisayad <sayad.mehrdad@gmail.com>
2025-11-02 01:52:05 +0330

    feat: add test for set methods and create candle method

--- tests/repository/shm/test_market_data_repository.py
+++ tests/repository/shm/test_market_data_repository.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time as record_time
 import pytest
 from src.fifi import MarketDataRepository
 from src.fifi.enums import Market
@@ -38,3 +39,35 @@ class TestMarketDataRepository:
 
         trade = repo.get_last_trade()
         assert trade == data[-1, MarketData.PRICE.value]
+
+    def test_set_methods(self, create_repo):
+        repo: MarketDataRepository = create_repo
+        data = np.arange(repo._rows * repo._columns).reshape(repo._rows, repo._columns)
+        repo._data = data
+
+        repo.set_close_price(12000)
+        assert repo._data[-1, MarketData.CLOSE.value] == 12000
+
+        repo.set_high_price(12500)
+        assert repo._data[-1, MarketData.HIGH.value] == 12500
+
+        repo.set_low_price(11500)
+        assert repo._data[-1, MarketData.LOW.value] == 11500
+
+        repo.set_open_price(12100)
+        assert repo._data[-1, MarketData.OPEN.value] == 12100
+
+        repo.set_last_trade(12300)
+        assert repo._data[-1, MarketData.PRICE.value] == 12300
+
+        t = int(record_time.time())
+        repo.set_time(t)
+        assert repo._data[-1, MarketData.TIME.value] == t
+
+    def test_creat_new_candle(self, create_repo):
+        repo: MarketDataRepository = create_repo
+        data = np.arange(repo._rows * repo._columns).reshape(repo._rows, repo._columns)
+        repo._data = data
+
+        repo.create_candle()
+        assert not repo._data[-1, :].any()
